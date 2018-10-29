a05aadbeabeb7942d097f60e288f5ae0183a61df
Bri Barnes <brianbarnes@umass.edu>
2018-10-29 11:17:57 -0400

    Test Script for Fetching Sites

--- coreScript.R
+++ coreScript.R
@@ -9,25 +9,16 @@
 
 library(Rcrawler)
 
+#####
+# Search Variables
+cvURL <- "https://chroniclevitae.com/job_search?utf8=%E2%9C%93&job_search%5Bkeywords%5D=&job_search%5Bposition_type%5D=&job_search%5Binstitution_type%5D=&job_search%5Bemployment_type%5D=&job_search%5Blocation%5D=&job_search%5Bzip_code%5D=&job_search%5Bdistance_from_zip%5D=10&job_search%5Bstart_date%5D=&job_search%5Bkeywords%5D=psychometrician"
+cbURL <- "https://www.careerbuilder.com/"
+
 
 #####
-# Crawling Scripts
-Rcrawler("https://www.indeed.com/jobs?q=Psychometrician", no_cores = 2, no_conn = 2, MaxDepth = 1)
+# Crawling Script
 
 
+Rcrawler(Website = cbURL, no_cores = 4, no_conn = 4, KeywordsFilter = c("psychometrician", "assessment", "testing"), KeywordsAccuracy = 70, MaxDepth = 2, DIR = "./test")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+LinkExtractor("https://www.careerbuilder.com")
